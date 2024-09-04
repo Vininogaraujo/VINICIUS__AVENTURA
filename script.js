3506834b79b740e5e7df772317c7fadf710dab22
Vininogaraujo <vininogueiradearaujo@gmail.com>
2024-09-04 17:29:05 -0300

    vininog

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelectorAAL('. btn-proximo');
+
+avanca.forEach(button =>){
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.atibvo')
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso). classList.add ('ativo')
+    } )
+}
